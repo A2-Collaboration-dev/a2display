208f1962852798b7d43c9137d38a037386b395f2
Oliver Steffen <steffen@kph.uni-mainz.de>
2014-11-14 14:08:48 +0100

    Added compliled_example

---
+++ compiled_example.cpp
@@ -0,0 +1,24 @@
+#include "TH2CB.h"
+
+#include <vector>
+#include "TRint.h"
+
+int main(int argc, char** argv) {
+
+    // Create the application object. Pass the command line arguments through.
+    TRint app( "AppTemplate", &argc, argv);
+
+    std::vector<double> v(720);
+
+    for(int i=0;i<v.size();++i) {
+        v.at(i)=i;
+    }
+
+    TH2CB* cb = new TH2CB();
+    cb->FillHitpattern720(v);
+    cb->Draw("text col");
+
+    app.Run();
+
+    return 0;
+}
